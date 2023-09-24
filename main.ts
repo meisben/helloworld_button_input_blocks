7af31a06a1a602fc1d04c6dd8426f780e5439d5e
Ben <28690361+meisben@users.noreply.github.com>
2023-09-24 12:08:39 +0100

    comments

--- main.ts
+++ main.ts
@@ -1,22 +1,23 @@
-//  If button A is pressed set the state variable to 0 
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
+/**
+ * Start the program, then run the forever loop
+ */
+/**
+ * If button B has been pressed, then show a sad face. Otherwise show a happy face
+ */
+// If button A is pressed set the state variable to 0
+input.onButtonPressed(Button.A, function () {
     state = 0
 })
-//  If button B is pressed set the state variable to 1
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+// If button B is pressed set the state variable to 1
+input.onButtonPressed(Button.B, function () {
     state = 1
 })
-//  If button B has been pressed, then show a sad face. Otherwise show a happy face
-//  Start the program, then run the forever loop
 let state = 0
 basic.showIcon(IconNames.Happy)
-basic.forever(function on_forever() {
+basic.forever(function () {
     if (state == 1) {
         basic.showIcon(IconNames.Sad)
     } else {
         basic.showIcon(IconNames.Happy)
     }
-    
 })
